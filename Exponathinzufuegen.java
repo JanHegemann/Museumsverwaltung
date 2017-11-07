3f07231ddde0a74e4433be6bf028555fbbb85179
Jan Hegemann <janhegemann1@gmail.com>
2017-11-07 09:21:13 +0100

    ghe

--- Exponathinzufuegen.java
+++ Exponathinzufuegen.java
@@ -10,6 +10,12 @@ public class Exponathinzufuegen implements ActionListener
     private JButton button1=new JButton();
     private Museum museum1=new Museum();
     private JLabel label1=new JLabel();
+    private ItemListener il;
+    private String status;
+    private Exponat exponat=new Exponat();
+    JTextField tf1=new JTextField ();
+    JTextField tf2=new JTextField ();
+    JTextField tf3=new JTextField ();
     public Exponathinzufuegen (){
         frame.add(panel);
         panel.add(label1);
@@ -17,11 +23,25 @@ public class Exponathinzufuegen implements ActionListener
         panel.setVisible(true);
         label1.setVisible(true);
         addButton(button1, "Exponat hinzufuegen");
-        JTextField tf1=new JTextField ();
-        JTextField tf2=new JTextField ();
-        JTextField tf3=new JTextField ();
         tf1.setText("Alter");
         tf2.setText("Kuenstler");
+        tf3.setText("Herkunft");
+        Choice wahl=new Choice();
+        panel.add(tf1);
+        panel.add(tf2);
+        panel.add(tf3);
+        panel.add(wahl);
+        wahl.add("Artefakt");
+        wahl.add("Bild");
+        wahl.add("Skulptur");
+        wahl.addItemListener(il);
+        tf1.addActionListener(this);
+        tf2.addActionListener(this);
+        tf3.addActionListener(this);
+        wahl.setVisible(true);
+        tf1.setVisible(true);
+        tf2.setVisible(true);
+        tf3.setVisible(true);
     }
 
     public static void main (){
@@ -38,6 +58,21 @@ public class Exponathinzufuegen implements ActionListener
     public void actionPerformed(ActionEvent e) { 
         if(e.getSource() == button1){ 
 
+        }
+        else if (e.getSource()==tf1){
+            
+        }
+        else if (e.getSource()==tf2){
+            
+        }
+        else if (e.getSource()==tf3){
+            
+        }
+    }
+    
+    public void itemStateChanged(ItemEvent e) {
+        if(e.getStateChange() == ItemEvent.SELECTED){
+            status=e.paramString();
         }
     }
 }
